79fac642176d4acf280fb36f114e647c71a3b93c
Stan Drozd <drozdziak1@gmail.com>
2022-06-07 18:47:50 +0200

    main.rs: change window title

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use kiss3d::window::Window;
 use kiss3d::light::Light;
 
 fn main() {
-    let mut window = Window::new("Kiss3d: cube");
+    let mut window = Window::new(concat!("Sphere Peeper ", env!("CARGO_PKG_VERSION")));
     let mut c      = window.add_cube(1.0, 1.0, 1.0);
 
     c.set_color(1.0, 0.0, 0.0);
