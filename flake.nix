775c56f8a899c753369057b0102f8c9b96f67856
Stan Drozd <drozdziak1@gmail.com>
2022-06-07 18:24:36 +0200

    flake init

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "A very basic flake";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/release-21.11;
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+
+  };
+}
