2b8af142d541c871bbda4bdf6f07b9e0d692afda
Stan Drozd <drozdziak1@gmail.com>
2022-06-07 18:43:18 +0200

    It compiles lol

--- flake.nix
+++ flake.nix
@@ -3,11 +3,16 @@
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/release-21.11;
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+    libs = (with pkgs; [libGL pkgconfig]) ++ (with pkgs.xorg; [libX11 libXcursor libXrandr libXi]);
+  in {
+    defaultPackage.${system} = pkgs.hello;
+    devShell.${system} = pkgs.mkShell {
+      nativeBuildInputs = libs;
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libs;
+    };
 
   };
 }
