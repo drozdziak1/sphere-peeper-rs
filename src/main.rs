1fa03d1cd9228b4f0a9d990a536c97d0e72cf712
Stan Drozd <drozdziak1@gmail.com>
2022-06-09 20:25:16 +0200

    Add a sphere and use the test pic on it

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,27 @@ extern crate kiss3d;
 
 use kiss3d::nalgebra as na;
 
-use na::{Vector3, UnitQuaternion};
-use kiss3d::window::Window;
 use kiss3d::light::Light;
+use kiss3d::window::Window;
+use na::{UnitQuaternion, Vector3};
 
-fn main() {
+use std::path::Path;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut window = Window::new(concat!("Sphere Peeper ", env!("CARGO_PKG_VERSION")));
-    let mut c      = window.add_cube(1.0, 1.0, 1.0);
+    let mut s = window.add_sphere(1.0);
 
-    c.set_color(1.0, 0.0, 0.0);
+    s.set_color(1.0, 1.0, 1.0);
 
     window.set_light(Light::StickToCamera);
 
     let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
 
+    s.set_texture_from_file(Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/test_pic.jpg")), "test-pic");
+
     while window.render() {
-        c.prepend_to_local_rotation(&rot);
+        s.prepend_to_local_rotation(&rot);
     }
+
+    Ok(())
 }
