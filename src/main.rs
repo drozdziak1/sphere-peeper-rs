c2b4f0314dbeaa907e81d451bd315ac433ea57ae
Stan Drozd <drozdziak1@gmail.com>
2022-06-09 21:37:24 +0200

    Add blender objects

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,42 @@
 extern crate kiss3d;
 
-use kiss3d::nalgebra as na;
+use kiss3d::{builtin::ObjectMaterial, nalgebra as na};
 
 use kiss3d::light::Light;
 use kiss3d::window::Window;
 use na::{UnitQuaternion, Vector3};
 
+use std::cell::RefCell;
 use std::path::Path;
+use std::rc::Rc;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut window = Window::new(concat!("Sphere Peeper ", env!("CARGO_PKG_VERSION")));
-    let mut s = window.add_sphere(1.0);
+    let mut front_half = window.add_obj(
+        Path::new("./sphere_hull.obj"),
+        Path::new("/dev/null"),
+        Vector3::new(1.0, 1.0, 1.0),
+    );
 
-    s.set_color(1.0, 1.0, 1.0);
+    let rot_front = UnitQuaternion::from_axis_angle(&Vector3::x_axis(), std::f32::consts::PI / -2.0);
 
-    window.set_light(Light::StickToCamera);
+    front_half.prepend_to_local_rotation(&rot_front);
+
+
+    front_half.set_color(1.0, 1.0, 1.0);
+    front_half.set_material(Rc::new(RefCell::new(Box::new(ObjectMaterial::new()))));
+
+    window.set_light(Light::Absolute(na::Point3::new(0.0, 0.0, 0.0)));
 
     let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
 
-    s.set_texture_from_file(Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/test_pic.jpg")), "test-pic");
+    front_half.set_texture_from_file(
+        Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/test_pic.jpg")),
+        "test-pic",
+    );
 
     while window.render() {
-        s.prepend_to_local_rotation(&rot);
+        front_half.prepend_to_local_rotation(&rot);
     }
 
     Ok(())
